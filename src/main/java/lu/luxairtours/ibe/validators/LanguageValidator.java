dfcff6b928c47d18ae3c608246bb7edc06ecf189
Othmane SENOUCI <consultant.othmane.senouci@luxairgroup.lu>
2020-01-13 08:55:22 +0100

    Added a language validator

---
+++ src/main/java/lu/luxairtours/ibe/validators/LanguageValidator.java
@@ -0,0 +1,24 @@
+package lu.luxairtours.ibe.validators;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class LanguageValidator implements
+ConstraintValidator<LanguageConstraint, String> {
+
+	private final static List<String> supportedLanguages = Arrays.asList(new String[]{"fr", "de", "en"});
+	
+	@Override
+    public void initialize(LanguageConstraint contactNumber) {
+    }
+	
+	@Override
+    public boolean isValid(String languageField,
+      ConstraintValidatorContext cxt) {
+		return supportedLanguages.contains(languageField.toLowerCase());
+    }
+	
+}
