9c2b8dbec3abe8e2f089db848ffa31833d4e1fd3
Othmane SENOUCI <consultant.othmane.senouci@luxairgroup.lu>
2020-02-11 16:39:52 +0100

    Fix the ports for the build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-mvn package && docker build -t registry.in.luxair.lu/osenouci/tavel-ibe-api .
+docker rmi $(docker images -q) --force
+mvn clean && mvn package && docker build -t registry.in.luxair.lu/osenouci/tavel-ibe-api .
